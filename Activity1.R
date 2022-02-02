0e0ff9b8e2d522ccdd2745735202e5f6ddae012a
egshaw <egshaw@colgate.edu>
2022-02-02 16:36:24 -0500

    I added question 2

--- Activity1.R
+++ Activity1.R
@@ -1,2 +1,13 @@
 #my new script
-print ("Hello World")
+
+#Question 2 - practicing vector operations in R
+#some unit vectors, i,j and k and a vector, l
+i <- (c(1L, 0L, 0L, 0L))
+j <- (c(0L, 1L, 0L, 0L))
+k <- (c(0L, 0L, 1L, 0L))
+l <- (c(3, 5, 7, sqrt(2)))
+# I don't know how I feel about the element-wise operations
+l %*% i
+l %o% k
+j + k
+i * j * k
