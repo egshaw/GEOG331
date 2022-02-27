df962ef887a750004e030ef7535f98a1d43f51f3
egshaw <egshaw@colgate.edu>
2022-02-27 12:38:38 -0500

    Finsihed questions 1-4.

--- Activity3.R
+++ Activity3.R
@@ -7,18 +7,25 @@ assert <- function(statement,err.message){
   
 }
 
-#check how the statement works
-#evaluate a false statement
-assert(1 == 2, "error: unequal values")
+######################################
+##          Question 1              ##
+######################################
+#The RH values are most accurate for humidity in the 25 to 65 percent range. The 
+# temperature range with the highest RH accuracy ranges from about 20 degrees C
+# to 60 degrees C. RH values are notably inaccurate at low temperatures (O degrees C)
+# and low humidity (0). Depending on the location of the sensor, this could give
+# a lot of NA values (e.g. this sensor would preform poorly in Hamilton, New York).
 
-#evaluate a true statement
-assert(2 == 2, "error: unequal values")
-#set up assert to check if two vectors are the same length
-a <- c(1,2,3,4)
-b <- c(8,4,5)
-assert(length(a) == length(b), "error: unequal length")
-
-##start body of activity
+######################################
+##          Question 2              ##
+######################################
+#Section 3.3 states "If an instantaneous measurement is more than eight times the running
+#average, the instantaneous measurement is rejected. It is not reported as the maximum gust
+#or included in the data that are averaged over the output interval." This is designed 
+# to prevent signal errors in wind speed calculation, and relies on the assumption that 
+# a factor of 8 is too large a change within a 100 second time span. Although this is 
+# likely included because the sensor would return faulty measurements otherwise, it seems
+# likely that in gusty weather, it would be possible to exceed this measurement legitimately.
 
 #use install.packages to install lubridate
 #install.packages(c("lubridate"))
@@ -44,6 +51,16 @@ colnames(datW) <-   colnames(sensorInfo)
 #preview data
 print(datW[1,])
 
+######################################
+##          Question 3              ##
+######################################
+# The argument "skip" ignores the first number of indicated lines. This is useful 
+#when the first few lines may contain headers and unit information that would otherwise
+#indicate to r to read the rest of the data as character data. nrows indicates the 
+#number of rows to read in. Thus, skip is exclusive and nrows is inclusive. The arg
+# header = FALSE indicates that the first row that will be read in contains data 
+#and does not contain row headers.
+
 #convert to standardized format
 #date format is m/d/y
 dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
@@ -53,8 +70,6 @@ datW$doy <- yday(dates)
 datW$hour <- hour(dates) + (minute(dates)/60)
 #calculate decimal day of year
 datW$DD <- datW$doy + (datW$hour/24)
-#quick preview of new date calcualtions
-datW[1,]
 
 #see how many values have missing data for each sensor observation
 #air temperature
@@ -68,13 +83,14 @@ length(which(is.na(datW$soil.moisture)))
 #soil moisture
 length(which(is.na(datW$soil.temp)))
 
-#make a plotof soil moisture with filled in points (using pch)
+#make a plot of soil moisture with filled in points (using pch)
 plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
      ylab="Soil moisture (cm3 water per cm3 soil)")
 
 #make a plot of air temperature with filled in points (using pch)
 plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
      ylab="Air temperature (degrees C)")
+
 
 #I'm going to make a new column to work with that indicates that I am conducting QAQC
 #because overwriting values should be done cautiously and can lead to confusing issues.
@@ -96,6 +112,16 @@ datW[datW$air.tempQ1 < 8,]
 #look at days with really high air temperature
 datW[datW$air.tempQ1 > 33,]
 
+######################################
+##          Question 4              ##
+######################################
+#Based on the wunderground data, our data seem pretty accurate. The measurements 
+#recorded in Rome for 6/22, 6/26, 7/1 and 7/2 are within a degree Celsius of our measurements
+# and seem to follow the same daily trends. The accuracy of the air temperature sensor does
+#not vary with humidity or solar radiation. So although relative humidity is higher for the 
+#days with low temperature, this does not indicate a higher chance of error. Overall, 
+# it seems like we can interprete these data as reliable.
+
 #plot precipitation and lightning strikes on the same plot
 #normalize lighting strikes to match precipitation
 lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
@@ -111,5 +137,4 @@ points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 
 #plot lightning points only when there is lightning     
 points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
        col= "tomato3", pch=19)
-
-##end of work 2/21/2022
+#end of work 2/27 12:38
