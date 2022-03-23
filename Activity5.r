9f1523053edea6e52ed6c3b9283e9227ffb4e7fe
egshaw <egshaw@colgate.edu>
2022-03-23 12:11:20 -0400

    end of work 3/23 12:11

---
+++ Activity5.r
@@ -0,0 +1,53 @@
+######################################
+###          Activity 5            ###
+###        Elizabeth Shaw          ###
+######################################
+
+#load in lubridate
+library(lubridate)
+
+#read in streamflow data and precipitation data (in mm)
+datH <- read.csv("Z://students/egshaw/Data/streamflow/stream_flow_data.csv", 
+                 na.strings = c("Eqp"))
+datP <- read.csv("Z://students/egshaw/Data/streamflow/2049867.csv")
+
+#only use measurements approved for publication
+datD <- datH[datH$discharge.flag == "A",]
+
+#### define time for streamflow #####
+#convert date and time
+datesD <- as.Date(datD$date, "%m/%d/%Y")
+#get day of year
+datD$doy <- yday(datesD)
+#calculate year
+datD$year <- year(datesD)
+#define time
+timesD <- hm(datD$time)
+
+#### define time for precipitation #####    
+dateP <- ymd_hm(datP$DATE)
+#get day of year
+datP$doy <- yday(dateP)
+#get year 
+datP$year <- year(dateP)
+
+#### get decimal formats #####
+#convert time from a string to a more usable format
+#with a decimal hour
+datD$hour <- hour(timesD ) + (minute(timesD )/60)
+#get full decimal time
+datD$decDay <- datD$doy + (datD$hour/24)
+#calculate a decimal year, but account for leap year
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/366),
+                       datD$year + (datD$decDay/365))
+#calculate times for datP                       
+datP$hour <- hour(dateP ) + (minute(dateP )/60)
+#get full decimal time
+datP$decDay <- datP$doy + (datP$hour/24)
+#calculate a decimal year, but account for leap year
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/366),
+                       datP$year + (datP$decDay/365))
+
+#plot discharge
+plot(datD$decYear, datD$discharge, type="l", xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
