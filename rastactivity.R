321791f1fb12044b992a6c413e333387091ec39b
egshaw <egshaw@colgate.edu>
2022-04-18 12:12:17 -0400

    end of work 4/18 (still have empty crs, maybe it works on a different computer?)

--- rastactivity.R
+++ rastactivity.R
@@ -38,5 +38,46 @@ b2 <- project(b,crs(p))
 b3 <- buffer(b2, width = 200)
 
 #use this to crop the raster layer so we can see just our study area
-p2 <- crop(p, b3,
+p2 <- crop(p, b3,overwrite = T,
            filename = "20190706_SR_crop.tif")
+
+#make a plot to see how it looks
+plotRGB(p2, r = 3, g = 2, b = 1,
+        scale = 65535,
+        stretch = "lin")
+
+#calculate NDVI
+#double brackets represent layer within spat raster/spat vector (i.e. this calculates
+#ndvi pixel by pixel)
+ndvi <- (p2[[4]] - p2[[3]])/(p2[[4]] + p2[[3]])
+
+#set the name layer to ndvi to avoid confusion
+names(ndvi) <- "ndvi"
+
+#create a plot of the ndvi map with sample points on top
+png(filename = "ndvi_map.png",
+    width = 6, height = 4, units = "in", res = 300)
+
+plot(ndvi)
+points(gtree2, cex = gtree$cc.pct/50, col = "blue")
+
+#stop writing to the file "ndvi_map.png"
+dev.off()
+
+#extract NDVI values for each point (careful with extract functions! dplyr also
+#has an exract function)
+nt <- terra:: extract(ndvi, gtree2, fun = mean, method = 'bilinear')
+
+#plot ndvi vs. canopy cover
+plot(nt$ndvi, gtree2$cc.pct,
+     pch = 16, col = "blue")
+
+#plot again but fix the y axis
+plot(nt$ndvi, gtree$cc.pct,
+     pch = 16, col = "blue",
+     xlim = c(0,1))
+
+
+
+
+
