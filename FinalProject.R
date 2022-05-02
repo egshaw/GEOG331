7963e33fe918bb157b3953228fb3ca8ecd9aa23b
egshaw <egshaw@colgate.edu>
2022-05-02 12:12:13 -0400

    Working on for loop rn

--- FinalProject.R
+++ FinalProject.R
@@ -10,7 +10,7 @@ tree.pecan <- read.csv("Z:/students/egshaw/Data/Final Project Data/individual_ph
 #creating a new dataframe with unique locations, save unique site ids
 site_ids <- unique(tree.pecan$Site_ID)
 lat_lon_df <- tibble(.rows = 221)
-lat_lon_df$site.ids <- tree.pecan$Site_ID
+lat_lon_df$id <- tree.pecan$Site_ID
 lat_lon_df$latitude <- tree.pecan$Latitude
 lat_lon_df$longitude <- tree.pecan$Longitude
 lat_lon_df$elevation <- tree.pecan$Elevation_in_Meters
@@ -21,29 +21,21 @@ lat_lon_df <- lat_lon_df %>% distinct()
 station_data <- ghcnd_stations()
 nearby_station_ids <- meteo_nearby_stations(lat_lon_df = lat_lon_df, station_data = station_data,
                                             lat_colname = "latitude", lon_colname = "longitude", 
-                                            var = c("TMIN", "TMAX", "TAVG", "PSUN"), 
+                                            var = c("TMIN", "TMAX", "TAVG", "PSUN"), limit = 1, 
                                             year_min = 2012, year_max = 2015, radius = 10)
 
+nearby_station_ids <- transpose(nearby_station_ids)
 
-#trying to split Phenophase Description column into three columns so it will be 
-#easy to distinguish leaf, bud, and fruit phenophases and compare them to each other.
-#tree.pecan <- tree.pecan %>% separate(col = Phenophase_Description, 
-  #                                    into = c("Phenophase_Leaves", "Phenophase_Flowers", "Phenophase_Fruits"))
-#this is the test which works
-station_data <- ghcnd_stations()
-lat_lon_df.test <- data.frame(id = c("sydney", "brisbane"),
-                         latitude = c(-33.8675, -27.4710),
-                         longitude = c(151.2070, 153.0234))
-nearby_stations <- meteo_nearby_stations(lat_lon_df = lat_lon_df.test, station_data = station_data,
-                                         lat_colname = "latitude", lon_colname = "longitude",
-                                         var = c("TMIN", "TMAX", "TAVG", "PSUN"), 
-                                         year_min = 2012, year_max = 2015, radius = 10)
+datW <- tibble(.rows = 4)
+for (i in 1:22){
+  if_else (is.na(nearby_station_ids[[1]][[i]]) = T, datW$i <- NA)
+  datW$i <- ghcnd_search(nearby_station_ids[[1]][[i]], date_min = "2012-01-01", date_max = "2015-12-31", 
+                         var = c("TMIN", "TMAX", "TAVG", "PSUN"))
+}
 
-#this is my attempt, which does not work 
-nearby_station_ids <- meteo_nearby_stations(tree.pecan, station_data = station_data,
-                                            lat_colname = "Latitude", lon_colname = "Longitude", 
-                                            var = c("TMIN", "TMAX", "TAVG", "PSUN"), 
-                                            year_min = 2012, year_max = 2015, radius = 1000)
+# ghcnd_search("USW00013891", date_min = "2012-01-01", date_max = "2015-12-31", 
+            # var = c("TMIN", "TMAX", "TAVG", "PSUN"))
+
 
 
 # color = State[Phenophase_Description == "Breaking leaf buds"]
